82822bbfe82b7b83dc037c8f7ed69c5e487f2063
Julio Reis <juliokreis@hotmail.com>
2021-01-10 14:21:54 -0300

    Estilizando com javaScript.

--- js/principal.js
+++ js/principal.js
@@ -21,12 +21,14 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log("Peso inválido!");
         pesoValido = false;
         tdImc.textContent = "Peso inválido!"
+        paciente.classList.add("paciente-invalido");
     }
     
     if (altura <= 0 || altura >= 3.00) {
         console.log("Altura inválido!");
         alturaValido = false;
         tdImc.textContent = "Altura inválida!"
+        paciente.classList.add("paciente-invalido");
     }
     
     if (pesoValido && alturaValida) {
