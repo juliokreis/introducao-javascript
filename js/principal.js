2e33381249a1f5e837e0f80f71484d1019a1a47f
Julio Reis <juliokreis@hotmail.com>
2021-01-10 20:47:41 -0300

    adicionando paciante à tabela (no HTML) com "document.createElement".

--- js/principal.js
+++ js/principal.js
@@ -28,19 +28,48 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log("Altura inválido!");
         alturaValido = false;
         tdImc.textContent = "Altura inválida!"
-        paciente.classList.add("paciente-invalido");
+        paciente.classList.add("paciente-invalido"); //lista as classes
     }
     
     if (pesoValido && alturaValida) {
         var imc = peso / (altura*altura);
-        tdImc.textContent = imc.toFixed(2);  
+        tdImc.textContent = imc.toFixed(2); //controla a qtd de casas decimais depois da virgula 
     }
 }
 
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
-    console.log("Fui clicado");
+botaoAdicionar.addEventListener("click", function(event){ //escuta um evento adicionado
+    event.preventDefault();// evita o comportamento padrão do formulário dentro da função anônima
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+  
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+        
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+    
 
 });
