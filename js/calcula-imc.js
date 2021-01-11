26bcb85bea83d7ad95f127362fe426d644c7325f
Julio Reis <juliokreis@hotmail.com>
2021-01-10 21:28:32 -0300

    Criou função

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -32,7 +32,19 @@ for (var i = 0; i < pacientes.length; i++) {
     }
     
     if (pesoValido && alturaValida) {
-        var imc = peso / (altura*altura);
-        tdImc.textContent = imc.toFixed(2); //controla a qtd de casas decimais depois da virgula 
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc; 
     }
 }
+
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2); //controla a qtd de casas decimais depois da virgula;
+}
+
+
+
